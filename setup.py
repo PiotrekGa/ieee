4ff944c21811ca6cb924ae23fbfa59c5535064fe
Piotr Gabrys <piotrek.gabrys@gmail.com>
2019-08-12 19:20:17 +0200

    reduce_mem func move to utils

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     author_email='piotrek.gabrys@gmail.com',
     version='0.0.1',
     packages=['ieee',],
+    install_requires=[
+        'scikit-learn',
+        'pandas',
+        'numpy'],
     license='MIT',
     long_description=open('README.md').read(),
     url="https://github.com/PiotrekGa/ieee"
