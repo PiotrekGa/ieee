9df5edad3807701bbb7d7411714142d99400adaa
Piotr Gabrys <piotrek.gabrys@gmail.com>
2019-08-12 18:37:49 +0200

    install option added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ieee',
+    author='Piotr Gabryś',
+    author_email='piotrek.gabrys@gmail.com',
+    version='0.0.1',
+    packages=['ieee',],
+    license='MIT',
+    long_description=open('README.md').read(),
+    url="https://github.com/PiotrekGa/ieee"
+)
